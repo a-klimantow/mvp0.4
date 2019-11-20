b727b1744f5d1e582dc20995059fe1a045f8d8a2
Anton Klimantov <klimantow@yandex.ru>
2019-11-20 13:50:37 +0300

    (Icon): create icons.json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react"
 
 import { StyledWrapper } from "styles"
-import { Text } from "./components"
+import { Text, Icon } from "./components"
 
 export const App = () => (
   <StyledWrapper>
     <Text as="h1" weight={600}>
       test hello
     </Text>
-    <Text weight={300} >test hello</Text>
+    <Text weight={300}>test hello</Text>
     <Text size="small">test hello</Text>
+    <Icon type='alarm'/>
+    <Icon type='down-arrow'/>
+    <Icon type="deadline"/>
+    <Icon type="company"/>
+    <Icon type="view_on"/>
+
   </StyledWrapper>
 )
--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,3 +1,21 @@
 import React from "react"
+import styled from "styled-components"
 
-export const Icon = () => null
+import icons from './icons.json'
+
+console.log(Object.keys(icons))
+console.log(icons)
+
+export const Icon = ({ type, ...props }) => {
+  return (
+    <Svg viewBox="0 0 16 16" {...props}>
+      <path fillRule="evenodd" clipRule="evenodd" d={icons[type]}/>
+    </Svg>
+  )
+}
+
+const Svg = styled.svg`
+  width: 16px;
+  height: 16px;
+  fill: currentColor;
+`
