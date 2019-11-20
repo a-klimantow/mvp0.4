87418482e97b77100ec54e503d864bccd7657c3e
Anton Klimantov <klimantow@yandex.ru>
2019-11-20 14:41:24 +0300

    (Layout): create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react"
 
 import { StyledWrapper } from "styles"
-import { Text } from "./components"
+import { Layout } from "./components"
 
 export const App = () => (
   <StyledWrapper>
-    <Text as="h3" weight={600} icon="resource_water">
-      Documents
-    </Text>
-    <Text icon="timer">test hello</Text>
+    <Layout>test</Layout>
   </StyledWrapper>
 )
