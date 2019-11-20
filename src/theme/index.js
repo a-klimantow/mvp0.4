944ea6182a4b2c5ce354ae73bea3964ef4dbf280
Anton Klimantov <klimantow@yandex.ru>
2019-11-20 15:39:16 +0300

    create theme folder & add colors

---
+++ src/theme/index.js
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    title: "#272F5A",
+    body: "rgba(39, 47, 90, 0.75)",
+    caption: "rgba(39, 47, 90, 0.45)",
+    disable: "rgba(39, 47, 90, 0.25)",
+    border: "rgba(39, 47, 90, 0.15)",
+    bg: "#F3F5F6",
+    bg_disabled: "rgba(39, 47, 90, 0.05)",
+    primary: "#189EE9",
+    secondary: "rgba(0, 194, 255, 0.15)",
+    water: {
+      cold: "#79AFFF",
+      hot: "#FF8C68"
+    },
+    electricity: "#E2B104",
+    error: "#ED3B45",
+    success: "#17B45A"
+  }
+}
